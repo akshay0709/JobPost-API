e3ea59f92d555f68e61598456c5f48500912c718
nehatambe <nehatambe1191@gmail.com>
2018-06-17 19:45:03 -0700

    pushing updated file

--- jobpost-search/src/test/java/com/jobpost/search/JobSearchVerticleTest.java
+++ jobpost-search/src/test/java/com/jobpost/search/JobSearchVerticleTest.java
@@ -21,10 +21,8 @@ import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
-
-
 @RunWith(VertxUnitRunner.class)
-public class JobSearchVerticleTest{
+public class JobSearchVerticleTest {
 	private Vertx vertx;
 
 	private static final Logger logger = LoggerFactory.getLogger(JobSearchVerticleTest.class);
@@ -32,14 +30,12 @@ public class JobSearchVerticleTest{
 	@Before
 	public void setUp(TestContext context) {
 		vertx = Vertx.vertx();
-		
-		JsonObject conf = new JsonObject().put("host","jobs-mysqlinstance.cuo0vhbk0bzr.us-west-1.rds.amazonaws.com")
-				.put("username","user")
-				.put("password","Test1234")	
-				.put("database","jobsDB")
-				.put("charset",	"UTF-8")
+
+		JsonObject conf = new JsonObject().put("host", "jobs-mysqlinstance.cuo0vhbk0bzr.us-west-1.rds.amazonaws.com")
+				.put("username", "user").put("password", "Test1234").put("database", "jobsDB").put("charset", "UTF-8")
 				.put("queryTimeout", 100000);
-		vertx.deployVerticle(JobSearchVerticle.class.getName(), new DeploymentOptions().setConfig(conf), context.asyncAssertSuccess());
+		vertx.deployVerticle(JobSearchVerticle.class.getName(), new DeploymentOptions().setConfig(conf),
+				context.asyncAssertSuccess());
 	}
 
 	@After
@@ -50,17 +46,14 @@ public class JobSearchVerticleTest{
 	@Test
 	public void testGetJobs(TestContext context) {
 		Async async = context.async();
-		
-		vertx.createHttpClient().get(8081, "localhost", "/jobs")
-				.putHeader("Content-Type", "application/json")
-				.putHeader("keyword","Java Developer")
-				.putHeader("location","US")
-				.handler(response -> {
+
+		vertx.createHttpClient().get(8081, "localhost", "/jobs").putHeader("Content-Type", "application/json")
+				.putHeader("keyword", "Java Developer").putHeader("location", "US").handler(response -> {
 					context.assertEquals(response.statusCode(), 200);
 					response.bodyHandler(body -> {
 						JsonArray result = body.toJsonArray();
 						logger.info(result);
-					
+
 						async.complete();
 					});
 				}).end();
